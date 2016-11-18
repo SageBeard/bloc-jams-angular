08c0bdca3af55daa815f01c8f0635ec44098c983
SageEnvisions <sagebeard@Sages-MacBook-Pro.local>
2016-11-17 18:44:50 -0800

    attempt-1

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+  function AlbumCtrl() {
+    this.albumData = []; {
+      this.albumData.push(angular.copy(albumPicasso));
+     }
+  }
+ 
+  angular
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
+ })();
+
+   
