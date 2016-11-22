724d1c9a52b9a1fed2f63113909510522f16e4ab
SageEnvisions <sagebeard@Sages-MacBook-Pro.local>
2016-11-21 18:21:44 -0800

    revised assn-5

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,12 @@
 (function() {
   function AlbumCtrl() {
-    this.albumData = []; {
-      this.albumData.push(angular.copy(albumPicasso));
-     }
+    this.albumData = angular.copy(albumPicasso);
   }
  
   angular
     .module('blocJams')
     .controller('AlbumCtrl', AlbumCtrl);
  })();
+
 
    
--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -10,3 +10,4 @@
     .module('blocJams')
     .controller('CollectionCtrl', CollectionCtrl);
  })();
+
